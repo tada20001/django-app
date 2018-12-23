b2c03b4fc01e9a8c6ad18185c6fa3e0fb73225e5
tada <tada20001@gmail.com>
2018-12-23 17:20:25 +0900

    rest_framework 직렬화

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
-
-router = DefaultRouter()
-router.register(r'post', PostViewSet)
+#from .views import PostViewSet
+from . import views
+#router = DefaultRouter()
+#router.register(r'post', PostViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    #path('', include(router.urls)),
+    path('post/', views.PostList.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
+#class PostViewSet(viewsets.ModelViewSet):
+class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
