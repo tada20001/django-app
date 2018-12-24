45f136b215e3a9b74d41fc54e3b34807ec9b14d3
tada <tada20001@gmail.com>
2018-12-24 15:01:54 +0900

    ep03 : generics APIView를 통한 로직 재사용¶

--- ep03/views.py
+++ ep03/views.py
@@ -8,27 +8,40 @@ from .models import Post
 from .serializers import PostSerializer
 
 
-class PostListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# class PostListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#     def get(self, request, *arg, **kwargs):
+#         return self.list(request, *arg, **kwargs)
+#
+#     def post(self, request, *arg, **kwargs):
+#         return self.create(request, *arg, **kwargs)
+
+# 위와 동일
+class PostListAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *arg, **kwargs):
-        return self.list(request, *arg, **kwargs)
 
-    def post(self, request, *arg, **kwargs):
-        return self.create(request, *arg, **kwargs)
 
 
 # /post/10/ ==> GET, PUT, DELETE
-class PostDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+
+# class PostDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+
+class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
