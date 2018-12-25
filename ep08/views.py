1c90a4943c74436cb4fff1c52b7ea9654febf254
tada <tada20001@gmail.com>
2018-12-26 00:56:51 +0900

    EP 08 : Serializer를 통한 저장 - DB로의 반영과 Mixins의 perform_ 계열 함수

--- ep08/views.py
+++ ep08/views.py
@@ -7,3 +7,7 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    # IP fields 자동 지정
+    def perform_create(self, serializer):
+        serializer.save(ip=self.request.META['REMOTE_ADDR'])
