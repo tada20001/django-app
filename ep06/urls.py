94e29df4f81221f5cdd2fdd8abf58497cb546084
tada <tada20001@gmail.com>
2018-12-25 17:25:49 +0900

    ep06 : add author field in ep04 app

--- ep06/urls.py
+++ ep06/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)
+
 
 app_name = 'ep06'
 
-urlpatterns = []
+urlpatterns = [
+    path('', include(router.urls))
+]
--- ep06/views.py
+++ ep06/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from ep04.models import Post
+from ep04.serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()  # 부모의 queryset을 호출
+        qs = qs.filter(title__icontains='team')
+        return qs
