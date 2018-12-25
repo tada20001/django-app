2802dc850eb3aa5874648fe011fcf29ef918ff15
tada <tada20001@gmail.com>
2018-12-25 23:07:55 +0900

    EP 08 : Serializer를 통한 유효성 검사

--- ep03/serializers.py
+++ ep03/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
+from rest_framework.exceptions import ValidationError
 from .models import Post
 
 
@@ -7,6 +8,18 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+# 한 필드에 대한 validation 처리 예 : 보통 모델에 구현하지만....
+    def validate_title(self, title):
+        if 'django' not in title:
+            raise ValidationError('제목에 django를 꼭 포함시켜 주세요.')
+        return title
+
+# 여러 필드를 한꺼번에 validation 처리할 때....
+    def validate(self, data):
+        if len(data['title']) % 2 == 0 or len(data['content']) % 2 == 0:
+            raise ValidationError('글자 갯수는 홀수로 지정해 주시기 바랍니다.')
+        return data
 
 
 class UserSerializer(ModelSerializer):
