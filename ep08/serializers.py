564128282c19c75986746e6262f72aaf4c1319b4
tada <tada20001@gmail.com>
2018-12-26 10:03:05 +0900

    ep09 : 사용자 인증 처리

--- ep08/serializers.py
+++ ep08/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
-        fields = ['author', 'message']
+        fields = ['message']
--- ep08/views.py
+++ ep08/views.py
@@ -8,6 +8,8 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    # post 저장시 현재 인증된 유저 저장
     # IP fields 자동 지정
     def perform_create(self, serializer):
-        serializer.save(ip=self.request.META['REMOTE_ADDR'])
+        if self.request.user.is_authenticated:
+            serializer.save(author=self.request.user, ip=self.request.META['REMOTE_ADDR'])
