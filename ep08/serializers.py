0fb611ac48277021c4a1454e45687284b1a29631
tada <tada20001@gmail.com>
2018-12-26 11:09:41 +0900

    ep09 : 포스팅 조회 응답에 작성자 필드 추가

--- ep08/serializers.py
+++ ep08/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
         model = Post
-        fields = ['message']
+        fields = ['author_username', 'message']
