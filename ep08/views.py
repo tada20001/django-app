bd656453d522178ae494a00a4cc85c0edc72341f
tada <tada20001@gmail.com>
2018-12-26 18:58:15 +0900

    ep12: 응답측정을 위한 코드 추가

--- ep08/views.py
+++ ep08/views.py
@@ -1,3 +1,4 @@
+import time
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
@@ -18,3 +19,34 @@ class PostViewSet(ModelViewSet):
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
             serializer.save(author=self.request.user, ip=self.request.META['REMOTE_ADDR'])
+
+    # dispatch time 측정을 위한 코드
+    def dispatch(self, request, *args, **kwargs):
+        global cbv
+        cbv = self
+
+        dispatch_start = time.time()
+        response = super().dispatch(request, *args, **kwargs)
+
+        render_start = time.time()
+        response.render()
+        self.render_time = time.time() - render_start
+
+        self.dispatch_time = time.time() - dispatch_start
+        self.api_view_time = self.dispatch_time - (self.render_time + self.serialize_time + self.db_time)
+
+        return response
+
+    # db time, serialize time 측정을 위한 코드
+
+    def list(self, request, *args, **kwargs):
+        db_start = time.time()
+        post_list = list(self.queryset)  # 현시점에서 즉시 DB fetch
+        self.db_time = time.time() - db_start
+
+        serializer_start = time.time()
+        serializer = self.get_serializer(self.queryset, many=True)
+        data = serializer.data
+        self.serializer_time = time.time() - serializer_start
+
+        return Response(data)
